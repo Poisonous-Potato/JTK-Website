0a66b56519d924cdf04a704ceb4f04ee10429718
reyrizki <reynaldirizki97.rr@gmail.com>
2022-05-20 06:56:03 +0700

    :lipstick: Add Banner in Faculty Page

--- portal-jtk/src/app/views/About/index.svelte
+++ portal-jtk/src/app/views/About/index.svelte
@@ -4,6 +4,8 @@
   import Section from "./section.svelte";
 
   import sections from "../../../lib/data/examples/About/about";
+
+  import banner from "../../../assets/profile_banner.png";
 </script>
 
 <MaterialApp>
@@ -12,7 +14,14 @@
       <h2>Jurusan Teknik Komputer dan Informatika</h2>
     </div>
   </Container>
+  <img id="banner" class="mb-16" src={banner} alt="Banner" />
   {#each sections as section}
     <Section content={section} />
   {/each}
 </MaterialApp>
+
+<style lang="scss">
+  #banner {
+    width: 100%;
+  }
+</style>
