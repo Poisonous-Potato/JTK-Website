508711c8314cf6a4782ea5936a0f081fc897d721
Muhammad Sakhi Hartanto <productionsakhi@gmail.com>
2022-05-19 19:58:53 +0700

    :speech_balloon: Add Comment on Config Cloudinary

--- cms-jtk/config/plugins.js
+++ cms-jtk/config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          cloud_name: env('CLOUDINARY_NAME'), //Setting up Cloudinary Name on .env
+          api_key: env('CLOUDINARY_KEY'), //Setting up Cloudinary API Key on .env
+          api_secret: env('CLOUDINARY_SECRET'), //Setting up Cloudinary API Secret on .env
         },
         actionOptions: {
           upload: {},
